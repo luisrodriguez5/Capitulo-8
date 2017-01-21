3c795d18062e76df1440f3f17a19914378542b73
Luis German Rodriguez Reynoso <luis_rodriguez5@ucne.edu.do>
2017-01-21 17:37:37 -0400

    ok

--- Ejercicio4/Program.cs
+++ Ejercicio4/Program.cs
@@ -10,6 +10,18 @@ namespace Ejercicio4
     {
         static void Main(string[] args)
         {
+            Console.WriteLine("Ingrese el primer numero: ");
+            int n1 = Convert.ToInt32(Console.ReadLine());
+            Console.WriteLine("Ingrese el primer numero: ");
+            int n2 = Convert.ToInt32(Console.ReadLine());
+            int Suma = n1 + n2;
+            String Result = " ";
+            if (Suma > 0)
+                Result = Suma.ToString();
+            else
+                Result = "(" + Suma + ")";
+            Console.WriteLine("La suma es igual a: " + Result);
+            Console.ReadKey();
 
         }
     }
