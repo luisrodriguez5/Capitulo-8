7ebce79748162e3cfac628c67c5cfb37b381b0e6
Luis German Rodriguez Reynoso <luis_rodriguez5@ucne.edu.do>
2017-01-21 17:33:40 -0400

    Finish

--- Ejercicio 2/Program.cs
+++ Ejercicio 2/Program.cs
@@ -10,6 +10,33 @@ namespace Ejercicio_2
     {
         static void Main(string[] args)
         {
+
+            String Cadena;
+            String Opcion;
+            int Op;
+            Console.WriteLine("Ingrese una Cadena: ");
+            Cadena = Console.ReadLine();
+            Console.WriteLine("Como desea justificar la cadena:\n1.Justificar hacia la izquierdad\n2.Justificar hacia la derecha");
+            Opcion = Console.ReadLine();
+            Op = Convert.ToInt32(Opcion);
+            if (Op == 1)
+            {
+                Console.WriteLine("la cadena Original: \n " + Cadena);
+                Cadena = Cadena.PadLeft(20);
+                Console.WriteLine("Cadena Justificada: \n" + Cadena);
+
+
+
+            }
+            if (Op == 2)
+            {
+
+                Console.WriteLine("la cadena Original: \n " + Cadena);
+                Cadena = Cadena.PadRight(20);
+                Console.WriteLine("Cadena Justificada: \n" + Cadena);
+            }
+
+            Console.ReadKey();
         }
     }
 }
